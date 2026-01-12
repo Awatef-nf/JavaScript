17dfba027919de680e2551fc23ab685eb284b546
awatef-toutou <awatefnf@hotmail.fr>
2026-01-12 09:23:47 +0100

    Deactivation de l ia

--- .idea/Jour2/Job3/script.js
+++ .idea/Jour2/Job3/script.js
@@ -6,30 +6,4 @@ ATTENTION : Vous ne devez pas utiliser “onclick()” dans votre HTML.
 _2
 La fonction permettant d’effectuer la modification doit s'appeler “addOne()”.*/
 
-document.addEventListener("DOMContentLoaded", function() {
-
-  //création des éléments
-    let nouveaubutton = document.createElement("button");
-    let nouveauCompteur = document.createElement("p");  
-    let compteur = 0;
-
-  //ajout des attributs et du texte
-    nouveaubutton.textContent="cliquez ici";
-    nouveaubutton.setAttribute("id","button");
-
-    nouveauCompteur.textContent = "0";
-    nouveauCompteur.setAttribute("id","compteur");  
-
-    //ajout au DOM
-    document.body.appendChild(nouveaubutton);
-    document.body.appendChild(nouveauCompteur);
-
-    //ajout de l'évènement
-    let bouton = document.getElementById('button');
-    bouton.addEventListener('click', function() {
-        let compteurP = document.getElementById('compteur');
-        compteur++;
-        compteurP.textContent = compteur;
-    });
-});
        
--- .idea/Jour2/Job4/script.js
+++ .idea/Jour2/Job4/script.js
@@ -3,25 +3,3 @@ Chaque fois que l’utilisateur tape une lettre sur son clavier (a-z), celle-ci 
 ajoutée dans le textarea (même si le focus en cours n’est pas le textarea).
 Si le focus en cours est dans le textarea, la lettre doit être ajoutée deux fois.*/
 
-document.addEventListener("DOMContentLoaded", function() {  
-    //création des éléments
-    let keylogger = document.createElement("textarea");
-    keylogger.setAttribute("id", "keylogger");
-
-    //ajout au DOM
-    document.body.appendChild(keylogger);
-
-    //ajout de l'évènement
-    document.addEventListener("keydown", function(event) {
-        if (event.key >= 'a' && event.key <= 'z') {
-            let textarea = document.getElementById('keylogger');
-            
-            if (document.activeElement === textarea) {
-                event.preventDefault();
-                textarea.value += event.key + event.key;
-            } else {
-                textarea.value += event.key;
-            }
-        }
-    });
-});
